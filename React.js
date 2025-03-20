e6661824b3eaa37052f42ee2cb7f4d66584de7a6
hcimtwb <jewelho@gmail.com>
2025-03-20 11:48:10 +0800

    Update React.js

--- React.js
+++ React.js
@@ -1,3 +1,8 @@
 ## React.JS - Deploy 3 modern web app
 Ref: https://www.youtube.com/watch?v=iKpkVKubvKk
+Skills: 
+- cache things, API, works with light mode vs dark mode, mobile vs desktop app
+- load it live on the screen
+- Firebase, Google's backend service to load things on server
+  
 
