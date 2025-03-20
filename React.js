229ae55db2a0a625cb80bb4f6a00f717921917f8
hcimtwb <jewelho@gmail.com>
2025-03-20 13:19:21 +0800

    Update React.js

--- React.js
+++ React.js
@@ -1,8 +1,21 @@
 ## React.JS - Deploy 3 modern web app
+
 Ref: https://www.youtube.com/watch?v=iKpkVKubvKk
 Skills: 
 - cache things, API, works with light mode vs dark mode, mobile vs desktop app
 - load it live on the screen
 - Firebase, Google's backend service to load things on server
-  
+- Authentification
+
+Watch project 1:
+High level structure: there are different folders
+- Parent components: App.jsx, contains <Header/>, <Tabs/>,<TodoList/>, <TodoInput/>
+- Child Components: Functional components: Header.jsx, Tabs.jsx, TodoCard.jsx, TodoInput.jsx, TodoList.jsx
+- Child child components: From TodoList.jsx, we have (multiple) <TodoCard/> embedded inside
+>> component content: contains a export function, and return jsx
+
+
+
+
+
 
