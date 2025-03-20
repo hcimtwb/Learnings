25050366bac1e0a707da0dd41bb56bde963f5581
hcimtwb <jewelho@gmail.com>
2025-03-20 13:27:30 +0800

    Update React.js

--- React.js
+++ React.js
@@ -9,11 +9,22 @@ Skills:
 
 Watch project 1:
 High level structure: there are different folders
-- Parent components: App.jsx, contains <Header/>, <Tabs/>,<TodoList/>, <TodoInput/>
-- Child Components: Functional components: Header.jsx, Tabs.jsx, TodoCard.jsx, TodoInput.jsx, TodoList.jsx
-- Child child components: From TodoList.jsx, we have (multiple) <TodoCard/> embedded inside
->> component content: contains a export function, and return jsx
 
+[1] src Folder: contains App.css, index.css, main.jsx
+- [1.1] Parent components: App.jsx, contains <Header/>, <Tabs/>,<TodoList/>, <TodoInput/>
+- [1.1] Child Components: Functional components: Header.jsx, Tabs.jsx, TodoCard.jsx, TodoInput.jsx, TodoList.jsx
+- [1.1] Child child components: From TodoList.jsx, we have (multiple) <TodoCard/> embedded inside
+>> component content: contains a export function, and return jsx
+- [1.2] main.jsx >> relates to [2] index.html
+>> createRoot(document.getElementById('root').render(
+   <StrictMode>
+      <App/>
+   </StrictMode>,)
+  
+[2] index.html file:
+- contains: 
+  <div id="root"></div>
+  <script type="module" src="/src/main.jsx"></script>
 
 
 
